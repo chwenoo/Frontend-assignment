5a007aa100bfd9c8b908faa5e88cef352bc656cc
Chwen <zzero.i.n.g@gmail.com>
2024-01-11 15:29:43 +0630

    no message

--- day2/js/formJs.js
+++ day2/js/formJs.js
@@ -1,6 +1,5 @@
 let errors = document.querySelectorAll('.error');
 let submit = document.querySelector('.submit');
-// let input = document.querySelectorAll('dd > input');
 let input = document.querySelectorAll('[data-require]');
 
 document.querySelector('.submit').addEventListener('click', (e) => {
@@ -10,7 +9,6 @@ document.querySelector('.submit').addEventListener('click', (e) => {
         if(input[x].value === "") {
             errors[x].style.display = "block";
             errors[x].innerText = `${input[x].name} is required.`;
-            // errors[x].innerText = `${input[x].name.replace(/-/g, " ")} is required.`;
         }
     }
 });
